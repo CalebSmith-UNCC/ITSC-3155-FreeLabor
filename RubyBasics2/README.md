7f7e1aa718e3aec912b1dad54edf22054de3d3ab
Kyle Long <klong57@uncc.edu>
2020-02-04 17:54:33 +0000

    Activity 6 initial commit

---
+++ RubyBasics2/README.md
@@ -0,0 +1,6 @@
+Ruby Basics
+=================
+
+The goal of this multi-part assignment is to get you accustomed to basic
+Ruby coding and introduce you to RSpec, the unit testing tool we will be
+using heavily.
---
+++ RubyBasics2/lib/rubybasics2.rb
@@ -0,0 +1,16 @@
+# Strings and Regular Expressions
+
+# Part I
+def hello(name)
+  # YOUR CODE HERE
+end
+
+# Part II
+def starts_with_consonant? s
+  # YOUR CODE HERE
+end
+
+# Part III
+def binary_multiple_of_4? s
+  # YOUR CODE HERE
+end
