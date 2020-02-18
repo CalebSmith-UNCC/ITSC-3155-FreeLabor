973e32afa9341622236000fa078a6c0c2ebb17a3
Kyle Long <klong57@uncc.edu>
2020-02-18 15:40:55 +0000

    Ruby OOP - Part 1

--- ruby_calisthenics2/lib/dessert.rb
+++ ruby_calisthenics2/lib/dessert.rb
@@ -2,7 +2,26 @@ class Dessert
   # add code for setters and getters
   def initialize(name, calories)
     # your code here
+    @name = name
+    @calories = calories
   end
+  
+  #getter and setter for name
+  def name=(name)
+  @name = name
+  end
+  def name
+    @name
+  end
+  
+  #getter and setter for calories 
+  def calories=(calories)
+  @calories = calories
+  end
+  def calories
+    @calories
+  end
+  
   def healthy?
     # your code here
   end
