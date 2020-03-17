648268e8cb60061f455f57d2de78abe391ae5750
Kyle Long <klong57@uncc.edu>
2020-03-17 02:04:42 +0000

    Complete Create and Show.

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,7 +1,22 @@
 class ArticlesController < ApplicationController
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    
     def new
     end
+    
+    
     def create
-        render plain: params[:article].inspect
+        @article = Article.new(article_params)
+        
+        @article.save
+        redirect_to @article
     end
 end
+
+private 
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
