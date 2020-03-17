b93672f830f29254eb4ca82c15e1ab96a9888d8b
Kyle Long <klong57@uncc.edu>
2020-03-17 03:20:12 +0000

    Add error messages for title validations

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -9,14 +9,19 @@ class ArticlesController < ApplicationController
     
     
     def new
+        @article = Article.new
     end
     
     
     def create
         @article = Article.new(article_params)
         
-        @article.save
-        redirect_to @article
+        if @article.save 
+            redirect_to @article 
+        else render 
+            'new' 
+        end
+
     end
 end
 
