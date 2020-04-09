268e857cec8d8470fe6b3be57de6c68ad26c86d5
CalebSmith-UNCC <csmit555@uncc.edu>
2020-04-09 17:23:04 +0000

    Added frame for User Stories

--- blog-test/spec/integration/blooger_adds_an_article_spec.rb
+++ blog-test/spec/integration/blooger_adds_an_article_spec.rb
@@ -20,3 +20,46 @@ feature"Blogger adds an article" do
         expect(page).to have_content("This is a new Capybara article")
     end
 end
+
+feature"Blogger edits an article" do
+    scenario "Blogger successfully navigates to the new articles page from the listing articles page" do
+        visit articles_path
+        expect(page).to have_content("Listing articles")
+        click_link "New article"
+        expect(page).to have_content("New Article")
+        expect(page).to have_field("Title")
+        expect(page).to have_field("Text")
+    end
+    
+    scenario "Blogger successfgully creates a new article" do
+        visit new_article_path
+        expect(page).to have_content("New Article")
+        fill_in "Title", with: "New Capybara Article"
+        fill_in "Text", with: "This is a new Capybara article"
+        click_button "Create Article"
+        expect(page).to have_content("New Capybara Article")
+        expect(page).to have_content("This is a new Capybara article")
+    end
+end
+
+feature"Blogger delete an article" do
+    scenario "Blogger successfully navigates to the new articles page from the listing articles page" do
+        visit articles_path
+        expect(page).to have_content("Listing articles")
+        click_link "New article"
+        expect(page).to have_content("New Article")
+        expect(page).to have_field("Title")
+        expect(page).to have_field("Text")
+    end
+    
+    scenario "Blogger successfgully creates a new article" do
+        visit new_article_path
+        expect(page).to have_content("New Article")
+        fill_in "Title", with: "New Capybara Article"
+        fill_in "Text", with: "This is a new Capybara article"
+        click_button "Create Article"
+        expect(page).to have_content("New Capybara Article")
+        expect(page).to have_content("This is a new Capybara article")
+    end
+end
+
