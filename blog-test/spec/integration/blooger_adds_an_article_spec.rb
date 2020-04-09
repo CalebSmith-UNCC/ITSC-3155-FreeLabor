5c5aa21feebf9a6bbe2848191dab46fa9da02206
ClamSageCaleb <caleb.benjamin9799@gmail.com>
2020-04-09 12:37:30 -0400

    Added Rspec and Capybara functionality

---
+++ blog-test/spec/integration/blooger_adds_an_article_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper.rb'
+
+feature"Blogger adds an article" do
+    scenario "Blogger successfully navigates to the new articles page from the listing articles page" do
+        visit articles_path
+        expect(page).to have_content("Listing articles")
+        click_link "New article"
+        expect(page).to have_content("New Article")
+        expect(page).to have_field("Title")
+        expect(page).to have_field("Text")
+    end
+    
+    scenario "Blogger successfgully creates a new article" do
+        visit new_article_path
+        expect(page).to have_content("New Article")
+        fill_in "Title", with: "New Capybara Article"
+        fill_in "Text", with: "This is a new Capybara article"
+        click_button "Create Article"
+        expect(page).to have_content("New Capybara Article")
+        expect(page).to have_content("This is a new Capybara article")
+    end
+end
