0ab10513b6d590ed4349472185526236c8d90ca2
Kyle Long <klong57@uncc.edu>
2020-04-10 12:30:32 +0000

    Added scenario for showing article

--- blog-test/spec/integration/blooger_adds_an_article_spec.rb
+++ blog-test/spec/integration/blooger_adds_an_article_spec.rb
@@ -10,7 +10,7 @@ feature"Blogger adds an article" do
         expect(page).to have_field("Text")
     end
     
-    scenario "Blogger successfgully creates a new article" do
+    scenario "Blogger successfully creates a new article" do
         visit new_article_path
         expect(page).to have_content("New Article")
         fill_in "Title", with: "New Capybara Article"
@@ -21,45 +21,50 @@ feature"Blogger adds an article" do
     end
 end
 
-feature"Blogger edits an article" do
-    scenario "Blogger successfully navigates to the new articles page from the listing articles page" do
-        visit articles_path
-        expect(page).to have_content("Listing articles")
-        click_link "New article"
-        expect(page).to have_content("New Article")
-        expect(page).to have_field("Title")
-        expect(page).to have_field("Text")
-    end
-    
-    scenario "Blogger successfgully creates a new article" do
+feature"Blogger Shows an article" do
+    scenario "Blogger successfully navigates to the show article page from the listing articles page" do
         visit new_article_path
         expect(page).to have_content("New Article")
         fill_in "Title", with: "New Capybara Article"
-        fill_in "Text", with: "This is a new Capybara article"
+        fill_in "Text", with: "This is a new capybara article"
         click_button "Create Article"
-        expect(page).to have_content("New Capybara Article")
-        expect(page).to have_content("This is a new Capybara article")
+        expect(page). to have_content("New Capybara Article")
+        expect(page). to have_content("This is a new capybara article")
+        visit articles_path
+        click_link "Show"
+        expect(page). to have_content("New Capybara Article")
+        expect(page). to have_content("This is a new capybara article")
     end
 end
 
-feature"Blogger delete an article" do
-    scenario "Blogger successfully navigates to the new articles page from the listing articles page" do
-        visit articles_path
-        expect(page).to have_content("Listing articles")
-        click_link "New article"
-        expect(page).to have_content("New Article")
-        expect(page).to have_field("Title")
-        expect(page).to have_field("Text")
-    end
-    
-    scenario "Blogger successfgully creates a new article" do
+feature"Blogger Edit an article" do
+    scenario "Blogger successfully navigates to the edit articles page from the listing articles page" do
         visit new_article_path
         expect(page).to have_content("New Article")
         fill_in "Title", with: "New Capybara Article"
-        fill_in "Text", with: "This is a new Capybara article"
+        fill_in "Text", with: "This is a new capybara article"
         click_button "Create Article"
-        expect(page).to have_content("New Capybara Article")
-        expect(page).to have_content("This is a new Capybara article")
+        expect(page). to have_content("New Capybara Article")
+        expect(page). to have_content("This is a new capybara article")
+        visit articles_path
+        click_link "Edit"
+        expect(page).to have_field("Title")
+        expect(page). to have_field("Text")
+    end
+    
+    scenario "Blogger successfully edits a new article" do
+        visit new_article_path
+        expect(page).to have_content("New Article")
+        fill_in "Title", with: "New Capybara Article"
+        fill_in "Text", with: "This is a new capybara article"
+        click_button "Create Article"
+        visit articles_path
+        click_link "Edit"
+        fill_in "Text", with: ""
+        fill_in "Text", with: "This is the updated capybara article"
+        click_button "Update Article"
+        expect(page). to have_content("New Capybara Article")
+        expect(page). to have_content("This is the updated capybara article")
     end
 end
 
