cf8658e48a5f545db5f79faa8b83d3f5033a4012
Kyle Long <klong57@uncc.edu>
2020-04-17 17:09:50 +0000

    Adding user functionality

--- blog-test/app/models/article.rb
+++ blog-test/app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+    belongs_to :user
+    
     has_many :comments
     validates :title, presence: true, length: { minimum: 5 }
     validates :text, presence: true, length: { minimum: 10 }
--- blog-test/config/routes.rb
+++ blog-test/config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :users
   get 'welcome/index'
   
   resources :articles do
     resources :comments
   end
+  
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
