020b8bb65f7f98aa9dd9eb54743c947bb59a2124
EC2 Default User <ec2-user@ip-172-31-86-163.ec2.internal>
2020-05-06 01:09:31 +0000

    Like/Favorite feature

--- blog-test/config/routes.rb
+++ blog-test/config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   
+  resources :posts do
+  resources :likes
+end
+  
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- blog-test/db/schema.rb
+++ blog-test/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_17_170056) do
+ActiveRecord::Schema.define(version: 2020_05_05_212957) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2020_04_17_170056) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
